bbb0f6a424897eb83256d364abde0dcd34e7eedb
Gashaw1 <akalc2004@yahoo.com>
2017-07-04 22:34:31 -0400

    .

---
+++ Building Messagaging  webservice/MessagingWebservice/MessageDataAccess/Models/Message.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MessageDataAccess.Models
+{
+    [Table("Message")]
+    public class Message
+    {
+        public Message()
+        {
+        }
+        public Message(string messagSenderID, string messageRecierID)
+        {
+            this.messagSenderID = messagSenderID;
+            this.messageRecierID = messageRecierID;
+        }
+        [Required]
+        [Key]
+        public int messageID { get; set; }
+        public string messages { get; set; }    
+        public DateTime messageDateTime { get; set; }
+        public string messagSenderID { get; set; }
+        public string messageRecierID { get; set; }
+            
+            
+    }
+}
